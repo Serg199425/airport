c5772263edf96eae39e95ef49bfb7ca20eb25465
serg199425 <serg199425@tut.by>
2015-07-29 04:31:24 +0300

    added seeds and some changes in active admin

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -1,5 +1,7 @@
 class Airplane < ActiveRecord::Base
   validates :registration_number, uniqueness: { case_sensitive: false }
   validates_presence_of :model_name, :registration_number
+  validates :model_name, length: { in: 2..100 }
+  validates :registration_number, length: { in: 6..30 }
   has_many :flights
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def random_string
+  (0...10).map { (65 + rand(26)).chr }.join + '-' + rand(1000..10000).to_s
+end
+
+Airplane.create(model_name: 'airbus A380', registration_number: random_string)
+Airplane.create(model_name: 'TU 144', registration_number: random_string)
+Airplane.create(model_name: 'TU 100', registration_number: random_string)
+Airplane.create(model_name: 'AH-2', registration_number: random_string)
+Airplane.create(model_name: 'boeing 737', registration_number: random_string)
+Airplane.create(model_name: 'boeing 747', registration_number: random_string)
+Airplane.create(model_name: 'AH-225', registration_number: random_string)
+Airplane.create(model_name: 'concord', registration_number: random_string)
+
+(0..15).each do |number|
+  User.create(email: 'user' + number.to_s + '@email.com', password: 'password')
+end
+
+AdminUser.create!(email: 'admin@mail.com', password: 'password', password_confirmation: 'password')
